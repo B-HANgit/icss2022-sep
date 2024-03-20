df3980a1a0a30b1058ed3accb5593059a13ecba8
B-HANgit <BAH.vanderVen@student.han.nl>
2024-03-20 19:57:29 +0100

    minor update to checker

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,7 +1,11 @@
 package nl.han.ica.icss.checker;
 
+import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.ASTNode;
+import nl.han.ica.icss.ast.VariableAssignment;
+import nl.han.ica.icss.ast.VariableReference;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -13,9 +17,21 @@ public class Checker {
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-        // variableTypes = new HANLinkedList<>();
+        variableTypes = new HANLinkedList<>();
 
+        walkTree(ast);
     }
 
-    
+    private void walkTree(AST ast) {
+        int depth = 0;
+        for(ASTNode node : ast.root.getChildren()) {
+            readNode(node, depth);
+        }
+    }
+
+    private void readNode(ASTNode node, int depth) {
+        for(ASTNode child : node.getChildren()) {
+            readNode(child, depth + 1);
+        }
+    }
 }
