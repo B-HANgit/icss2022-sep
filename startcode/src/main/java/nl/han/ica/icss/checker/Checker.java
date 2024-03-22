f150c08e3b29ceb553cb7c7f8df955e1d810a735
B-HANgit <BAH.vanderVen@student.han.nl>
2024-03-22 16:00:09 +0100

    Update Checker.java
    + almost all implemented (see TODO)

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -7,6 +7,7 @@ import nl.han.ica.icss.ast.ASTNode;
 import nl.han.ica.icss.ast.VariableAssignment;
 import nl.han.ica.icss.ast.VariableReference;
 import nl.han.ica.icss.ast.literals.*;
+import nl.han.ica.icss.ast.operations.MultiplyOperation;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -14,62 +15,109 @@ import java.util.HashMap;
 import nl.han.ica.icss.ast.*;
 
 
+//TODO    Controleer of de operanden van de operaties plus en min van gelijk type zijn. Je mag geen pixels bij percentages optellen bijvoorbeeld.
+//        Controleer dat bij vermenigvuldigen minimaal een operand een scalaire waarde is. Zo mag 20% * 3 en 4 * 5 wel, maar mag 2px * 3px niet.
+
 public class Checker {
 
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
     private int scope = 0;
     public void check(AST ast) {
         variableTypes = new HANLinkedList<>();
-
-        walkTree(ast.root, scope);
-
-        printAllVariables();
+        variableTypes.insert(scope, new HashMap<>());
+        walkTree(ast.root);
     }
 
-    private void walkTree(ASTNode node, int depth) {
-            // Create a new variable scope for stylesheets
-            variableTypes.insert(depth, new HashMap<>());
+    private void walkTree(ASTNode node) {
         if (node instanceof VariableAssignment) {
-            checkVariableAssignment(depth, (VariableAssignment) node);
-        }
-//        else if (node instanceof Declaration) {
-//            checkDeclaration((Declaration) node);
-//        } else if (node instanceof IfClause) {
-//            checkIfStatement((IfClause) node);
-//        } else if (node instanceof Operation) {
-//            checkOperation((Operation) node);
-//        }
+            checkVariableAssignment(scope, (VariableAssignment) node);
+            childWalkTree(node);
 
-        // Recursively check children nodes
-        for (ASTNode child : node.getChildren()) {
-//            System.out.println("Depth: " + depth + " - " + child.getClass().getSimpleName());
-            walkTree(child, depth + 1);
+            printVariables(scope);
+        }
+        else if (node instanceof IfClause) {
+            createNewScope();
+            checkIfStatement((IfClause) node);
+            childWalkTree(node);
+            deleteScope();
+        } else if (node instanceof Stylerule) {
+            createNewScope();
+            childWalkTree(node);
+            deleteScope();
+        }
+        else if (node instanceof Declaration) {
+            checkDeclaration((Declaration) node);
+            childWalkTree(node);
+        }
+        else if (node instanceof Operation) {
+            checkOperation((Operation) node);
+            childWalkTree(node);
+        }
+        else{
+            childWalkTree(node);
+        }
+    }
+
+    private void checkDeclaration(Declaration node) {
+        ExpressionType valueType = getType(node.expression);
+        String propertyName = node.property.name.toLowerCase();
+        if(propertyName.equals("color") || propertyName.equals("background-color")){
+            if(valueType != ExpressionType.COLOR){
+                node.setError("Value of property " + propertyName + " should be of type color");
+            }
+        }
+        else if(propertyName.equals("width") || propertyName.equals("height")){
+            if(valueType != ExpressionType.PIXEL || valueType != ExpressionType.PERCENTAGE){
+                node.setError("Value of property " + propertyName + " should be of type pixel");
+            }
+        }
+    }
+
+    //TODO een scalar*scalar kan alleen in een var-calc niet in een prop-calc
+    private void checkOperation(Operation node) {
+        ExpressionType leftType = getType(node.lhs);
+        ExpressionType rightType = getType(node.rhs);
+
+        if (leftType == ExpressionType.UNDEFINED) {
+            node.setError("Left-hand side of operation is undefined");
+        }
+        if (rightType == ExpressionType.UNDEFINED) {
+            node.setError("Right-hand side of operation is undefined");
+        }
+        if (leftType == ExpressionType.COLOR || rightType == ExpressionType.COLOR) {
+            node.setError("Cannot perform operation with color");
+        }
+        if (leftType == ExpressionType.BOOL || rightType == ExpressionType.BOOL) {
+            node.setError("Cannot perform operation with boolean");
         }
 
-        // Remove the current variable scope
-//        variableTypes.delete(depth);
+        if (node instanceof MultiplyOperation) {
+            if (leftType != ExpressionType.SCALAR && rightType != ExpressionType.SCALAR) {
+                node.setError("At least one operand of multiplication should be a scalar");
+            }
+        } else {
+            if (leftType != rightType) {
+                node.setError("Operands of operation are not of the same type");
+            }
+            if (leftType == ExpressionType.SCALAR || rightType == ExpressionType.SCALAR){
+                node.setError("Cannot perform operation (plus/min) with scalar");
+            }
+        }
     }
 
 
-//TODO    Controleer of er geen variabelen worden gebruikt die niet gedefinieerd zijn.
-
-//TODO    Controleer of de operanden van de operaties plus en min van gelijk type zijn. Je mag geen pixels bij percentages optellen bijvoorbeeld.
-//        Controleer dat bij vermenigvuldigen minimaal een operand een scalaire waarde is. Zo mag 20% * 3 en 4 * 5 wel, maar mag 2px * 3px niet.
-
-//TODO    Controleer of er geen kleuren worden gebruikt in operaties (plus, min en keer).
-
-//TODO    Controleer of bij declaraties het type van de value klopt met de property. Declaraties zoals width: #ff0000 of color: 12px zijn natuurlijk onzin.
-
-//TODO    Controleer of de conditie bij een if-statement van het type boolean is (zowel bij een variabele-referentie als een boolean literal)
-
-//TODO    Controleer of variabelen enkel binnen hun scope gebruikt worden
+    private void childWalkTree(ASTNode node){
+        for (ASTNode child : node.getChildren()) {
+            walkTree(child);
+        }
+    }
 
     private void checkVariableAssignment(int depth, VariableAssignment assignment) {
         String variableName = assignment.name.name;
         ExpressionType valueType = getType(assignment.expression);
 
         if(valueType == ExpressionType.UNDEFINED) {
-            System.out.println("Error: Variable " + variableName + " has an undefined type");
+            assignment.setError("Variable " + variableName + " has an undefined type");
         }
 
         // Check if the variable is already defined in the current scope
@@ -79,6 +127,17 @@ public class Checker {
         } else {
             // Variable not defined, add it to the current scope
             variableTypes.get(depth).put(variableName, valueType);
+        }
+    }
+
+    private void checkIfStatement(IfClause node) {
+        ExpressionType conditionType = getType(node.conditionalExpression);
+
+        if(conditionType == ExpressionType.UNDEFINED) {
+            node.setError("Condition in if-statement is undefined");
+        }
+        if (conditionType != ExpressionType.BOOL) {
+            node.setError("If statement condition is not of type boolean");
         }
     }
 
@@ -95,7 +154,11 @@ public class Checker {
             return ExpressionType.BOOL;
         } else if (expression instanceof VariableReference) {
             String variableName = ((VariableReference) expression).name;
-            return getVariableType(variableName);
+            ExpressionType expressionType = getVariableType(variableName);
+            if(expressionType == ExpressionType.UNDEFINED){
+                expression.setError("Variable " + variableName + " is not defined (at this scope)");
+            }
+            return expressionType;
         }
         return ExpressionType.UNDEFINED;
     }
@@ -107,5 +170,19 @@ public class Checker {
             }
         }
         return ExpressionType.UNDEFINED;
+    }
+
+    private void createNewScope() {
+        scope++;
+        variableTypes.insert(scope, new HashMap<>());
+    }
+
+    private void deleteScope() {
+        variableTypes.delete(scope);
+        scope--;
+    }
+
+    private void printVariables(int scope) {
+        System.out.println("Scope " + scope + ": " + variableTypes.get(scope).toString());
     }
 }
